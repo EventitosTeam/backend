eed5ad94fb631f81f4ea5156ce038a6b4dac7d75
Bruno Romero <65194372+bruno290300@users.noreply.github.com>
2024-06-27 11:01:18 -0300

    chore: Update users_controller.py

--- swagger_server/controllers/users_controller.py
+++ swagger_server/controllers/users_controller.py
@@ -4,51 +4,45 @@ import six
 from swagger_server.models.book_item import BookItem  # noqa: E501
 from swagger_server.models.guest_item import GuestItem  # noqa: E501
 from swagger_server import util
-from flask import Blueprint, request
+
+
+from flask import Blueprint, request, jsonify
 from swagger_server.schemas.book_schema import BookSchema
 from swagger_server.services.book_service import BookService
 from swagger_server.schemas.event_schema import EventSchema
 from swagger_server.services.event_service import EventService
-from flask import request, jsonify
 from swagger_server.mail.function import send_mail
+
+
 
 events = Blueprint('events', __name__)
 bookings = Blueprint('bookings', __name__)
+
+
+
 book_service = BookService()
 book_schema = BookSchema()
-
 event_service = EventService()
 event_schema = EventSchema()
 
-def delete(booking_code):  # noqa: E501
-    """Unregister from an event
 
-     # noqa: E501
 
-    :param booking_code: booking code
-    :type booking_code: 
-
-    :rtype: None
-    """
-    return 'do some magic!'
+@bookings.route('/<string:booking_code>', methods=['DELETE'])
+def delete_booking(booking_code):
+    """Unregister from an event"""
+    result = book_service.delete_booking(booking_code)
+    if result:
+        return '', 204
+    else:
+        return jsonify({'detail': f'Booking with code {booking_code} not found'}), 404
 
 @bookings.route('/<string:booking_code>', methods=['GET'])
-def get_event_enrolled(booking_code):  # noqa: E501
-    """Get a user enrolled by userID
-
-     # noqa: E501
-
-    :param booking_code: booking code
-    :type booking_code: str
-
-    :rtype: BookItem
-    """
-
+def get_event_enrolled(booking_code):
+    """Get a user enrolled by userID"""
     return book_schema.dump(book_service.get_event_enrolled(booking_code))
 
-# /events/{event_id}/bookings
 @events.route('/<int:event_id>/bookings', methods=['POST'])
-def post_book(event_id):  # noqa: E501
+def post_book(event_id):
     user = request.get_json()
     book = {
         "user": str(user),
@@ -60,17 +54,13 @@ def post_book(event_id):  # noqa: E501
     return book_schema.dump(book_service.add_event_book(book, event_id))
 
 @events.route('/<int:id>', methods=['GET'])
-def get_event_by_id(id):  # noqa: E501
-    """Get a specific event by ID
-
-     # noqa: E501
-
-    :param id: ID of the event to retrieve
-    :type id: 
-
-    :rtype: EventItem
-    """
-    return event_schema.dump(event_service.get_event_by_id(id))
+def get_event_by_id(id):
+    """Get a specific event by ID"""
+    event = event_service.get_event_by_id(id)
+    if event:
+        return event_schema.dump(event)
+    else:
+        return jsonify({'detail': f'Event with id {id} not found'}), 404
 
 @events.route('/', methods=['POST'])
 def create_event():
@@ -82,11 +72,10 @@ def create_event():
     return event_service.add_event(event)
 
 @events.route('/', methods=['GET'])
-def search_events():  # noqa: E501
+def search_events():
     """searches events
 
-    By passing in the appropriate options, you can search for available events in the system  # noqa: E501
-
+    By passing in the appropriate options, you can search for available events in the system
 
     :rtype: List[EventItem]
     """

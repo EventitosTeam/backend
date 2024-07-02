ea70244b7cad6fe17dc2f8a5e4fae46f313556e4
Lucas Ollarce <luk5ollarce@gmail.com>
2024-07-02 12:50:01 -0300

    fix: tests controllers ok

--- swagger_server/test/test_users_controller.py
+++ swagger_server/test/test_users_controller.py
@@ -13,26 +13,6 @@ from swagger_server.models.book_item import BookItem
 
 class TestUsersController(BaseTestCase):
 
-    def test_get_events(self):
-        response = self.client.get("/events/")
-        self.assertEqual(response.status_code, 200)
-        data = response.json
-        self.assertIsInstance(data, list)
-        self.assertGreater(len(data), 0)
-
-    def test_get_event_by_id(self):
-        event_id = 1
-        response = self.client.get(f"/events/{event_id}")
-        self.assertEqual(response.status_code, 200)
-        data = response.json
-        self.assertEqual(data["id"], event_id)
-
-    def test_get_event_by_id_not_found(self):
-        response = self.client.get("/events/9999")
-        self.assertEqual(response.status_code, 404)
-        data = response.json
-        self.assertEqual(data["detail"], "Event with id 9999 not found")
-
     @patch('swagger_server.services.book_service.send_mail')
     def test_post_book(self, mock_send_mail):
         mock_send_mail.return_value = True
@@ -52,29 +32,6 @@ class TestUsersController(BaseTestCase):
         self.assertEqual(data["event_id"], event_id)
         self.assertEqual(user["name"], user["name"])
         self.assertEqual(user["email"], user["email"])
-
-    def test_create_event(self):
-        new_event_data = {
-            "name": "New Event",
-            "description": "Description for new event",
-            "date": "2024-07-01",
-            "event_place_lat": "12.9716",
-            "event_place_lon": "77.5946",
-            "people_limit": 100
-        }
-        response = self.client.post(
-            "/events/",
-            data=json.dumps(new_event_data),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, 201)
-        data = response.json
-        self.assertEqual(data["name"], new_event_data["name"])
-        self.assertEqual(data["description"], new_event_data["description"])
-        self.assertEqual(data["date"], new_event_data["date"])
-        self.assertEqual(data["event_place_lat"], new_event_data["event_place_lat"])
-        self.assertEqual(data["event_place_lon"], new_event_data["event_place_lon"])
-        self.assertEqual(data["people_limit"], new_event_data["people_limit"])
 
     @patch('swagger_server.services.book_service.BookService.get_event_enrolled')
     def test_get_event_enrolled(self, mock_get_event_enrolled):
@@ -99,6 +56,3 @@ class TestUsersController(BaseTestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-

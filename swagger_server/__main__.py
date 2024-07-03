d9078efe3054fe0f126cf51969efb4e7300e6b1f
DouglasArenas <douglasarenas71@gmail.com>
2024-07-03 18:30:18 -0300

    chore: Remove unused imports and comments in __main__.py

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
-# import connexion
 import pymysql
-
-# from swagger_server import encoder
-# from flask_sqlalchemy import SQLAlchemy
 from swagger_server.database import db
-
-# db = SQLAlchemy()
 import logging
 from flask import Flask
 from swagger_server.models import EventItem, BookItem, GuestItem
@@ -17,10 +11,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def main():
-
-
     logger.info("Inicializando la aplicación Flask")
-    print('hola chau sss')
 
     HOST = "monorail.proxy.rlwy.net"
     USER = "root"
@@ -28,11 +19,7 @@ def main():
     PORT = 32365
     DB_NAME = "railway"
 
-
     app = Flask(__name__)
-    # app = connexion.App(__name__, specification_dir='./swagger/')
-
-    # flask_app = app.app
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['TESTING'] = True
@@ -45,11 +32,6 @@ def main():
     # Crear todas las tablas en la base de datos
     with app.app_context():
         db.create_all()
-    # db.drop_all()
-
-    # app.app.json_encoder = encoder.JSONEncoder
-    # app.add_api('swagger.yaml', arguments={'title': 'API para sistema de Eventos'}, pythonic_params=True)
-    # app.run(port=8080)
 
     from swagger_server.controllers.users_controller import events, bookings
     app.register_blueprint(events, url_prefix='/events')
@@ -65,7 +47,6 @@ def main():
         return 'OK'
 
     app.run(host = '0.0.0.0', debug = True, port = 8080)
-
 
 if __name__ == '__main__':
     main()

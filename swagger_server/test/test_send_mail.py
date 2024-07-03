db4a3d47026e5bd4d9c0f138f3c98cb96cd9ad04
DouglasArenas <douglasarenas71@gmail.com>
2024-07-03 19:41:11 -0300

    chore: Update send_mail function, finished send mail test and deleted admin test

--- swagger_server/test/test_send_mail.py
+++ swagger_server/test/test_send_mail.py
@@ -1,4 +1,5 @@
-import unittest, os, sys
+import unittest
+import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from unittest.mock import patch
 from io import StringIO
@@ -9,32 +10,26 @@ class TestSendMail(unittest.TestCase):
 
     @patch('smtplib.SMTP')
     def test_send_mail(self, mock_smtp):
-        # Configurar el mock SMTP
         instance = mock_smtp.return_value
         instance.starttls.return_value = (220, 'Service ready')
         instance.login.return_value = (235, 'Authentication successful')
-        to_address = 'recipient@example.com'
+        to_address = 'douglasarenas71@gmail.com'
         subject = 'Testing email sending'
         body = 'This is a test email.'
         send_mail(to_address, subject, body)
         instance.sendmail.assert_called_once()
         call_args = instance.sendmail.call_args[0]
-        self.assertIn(to_address, call_args[0])
-        self.assertIn(subject, call_args[1])
+        self.assertEqual(to_address, call_args[0])
+        self.assertIn(subject, call_args[2])
         self.assertIn(body, call_args[2])
 
     @patch('smtplib.SMTP')
     def test_smtp_error_handling(self, mock_smtp):
-        # Configurar el mock SMTP para simular un error de conexión
         instance = mock_smtp.return_value
         instance.starttls.side_effect = smtplib.SMTPException('StartTLS failed')
-
-        # Definir variables para la prueba
         to_address = 'recipient@example.com'
         subject = 'Testing email sending'
         body = 'This is a test email.'
-
-        # Llamar a la función de enviar correo y capturar excepciones
         with self.assertRaises(smtplib.SMTPException):
             send_mail(to_address, subject, body)
 
